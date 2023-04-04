39a43a252f39cd68986493cdadd5543201326fdc
madyanes <madyanes.1995@gmail.com>
2023-04-05 02:29:51 +0700

    create back to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,15 @@ const buttonElement = document.querySelectorAll('.button')
 buttonElement.forEach((element) => {
   element.addEventListener('click', inDevlepmentMessage)
 })
+
+const topButton = document.getElementById('top')
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = 'block'
+  } else {
+    topButton.style.display = 'none'
+  }
+}
+
+window.onscroll = () => scrollFunction()
