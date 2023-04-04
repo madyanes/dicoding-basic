7e53dadbc6e2e84f2576d467c04d668472cc0b72
madyanes <madyanes.1995@gmail.com>
2023-04-05 01:16:14 +0700

    alert message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,12 @@
 const date = new Date()
 document.querySelector('footer #copyright').innerHTML = 'Copyright &copy; ' + date.getFullYear()
+
+const inDevlepmentMessage = (e) => {
+  e.preventDefault()
+  alert('Mohon maaf! Fitur ini dalam tahap pengembangan, belum dapat digunakan.')
+}
+
+const buttonElement = document.querySelectorAll('.button')
+buttonElement.forEach((element) => {
+  element.addEventListener('click', inDevlepmentMessage)
+})
